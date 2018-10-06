95949b80c70721f2ea7449aa16a6bd21d20a6511
Samkelo Gigaba <43130772+sgigaba@users.noreply.github.com>
2018-10-06 15:58:40 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,20 +4,49 @@
     <link rel = "stylesheet" type = "text/css" href = "styles/styles.css">
     <title>indexpage</title>
 </head>
-<body class = "bc">
+<body background = "https://data.whicdn.com/images/22697191/original.jpg">
 <div class = "maindiv"> 
 <div class = "hclass">
-    <img id = "imgc" src = "http://webneel.com/sites/default/files/images/manual/logo/10-best-shopping-store-logo.gif"/>
+
+<div class = "signupl">
+    <button type = "button">SIGN IN</button>
+    <button type = "button">SIGN UP</button>
+</div>
+<!-- <div class = "login.c">
+    <form>
+        Name = <input type = "text", name = "username"> 
+        Password = <input type = "text", name = "password">
+       </form>
+</div> -->
+       
+       
+    <a href = "checkout.php"><img id = "imgc" src = "https://cdn4.iconfinder.com/data/icons/shopping-in-color/64/shopping-in-color-05-512.png"></a>
        <h1>Menubar</h1>
 </div>
-<div>   
-<div class = "contentc">
-        sidebar
+<div class = "content">   
+<div class = "sidebar">
+        <h1 class = "head"> Categories </h1>
+        <br>
+        <div class = "box">
+            Sandals
+        </div>
+        <br>
+        <div class = "box">
+            <a href = "sneakers.php"> Sneakers </a>
+        </div>
+        <br>
+        <div class = "box">
+            Boots
+        </div>
+        <br>
+        <div class = "box">
+            Sportswear
+        </div>
 </div>
     <div>
         content area
-</div>
-<div>
+    </div>
+<div class = "footer">
         footer
     </div>
 </div>
---
+++ sneakers.php
@@ -0,0 +1,40 @@
+
+<html>
+<head>
+    
+    <title>Sneakers</title>
+    <link rel="stylesheet" type="text/css" href = "styles/styles.css"/>
+    <div class = "this">
+
+    <div class = "signupl">
+    <button type = "button">SIGN IN</button>
+    <button type = "button">SIGN UP</button>
+    </div>
+    <img id = "imgc" src = "https://cdn4.iconfinder.com/data/icons/shopping-in-color/64/shopping-in-color-05-512.png">
+        <h1>
+
+        </h1>
+    </div>
+</head>
+<body background = "https://www.nipponandco.fr/8570-large_default/tissu-noir-japonais-en-coton-motifs-fleurs-fabrique-au-japon-largeur-110-cm-x-1m.jpg">
+<div class = "maindiv"> 
+<div class = "content">   
+    <div>
+    <div class = "sneakerbox">
+            <img class = "simgbox" src = "https://assets.superbalistcdn.co.za/500x720/filters:quality(75):format(jpg)/650480/original.jpg"/> 
+            <img class = "simgbox" src = "https://assets.superbalistcdn.co.za/500x720/filters:quality(75):format(jpg)/654783/original.jpg"/> 
+            <img class = "simgbox" src = "https://assets.superbalistcdn.co.za/500x720/filters:quality(75):format(jpg)/624378/original.jpg"/>
+            <img class = "simgbox" src = "https://assets.superbalistcdn.co.za/500x720/filters:quality(75):format(jpg)/spree/product/s/p/spree2018may02lmmvds88.jpg"/>
+            <img class = "simgbox" src = "https://assets.superbalistcdn.co.za/500x720/filters:quality(75):format(jpg)/558328/original.jpg"/> 
+        </div>
+        <br>  
+    <div class = "more information">    
+
+    
+    </div>
+    </div>
+</div>
+</div>
+</body>
+
+</html>
