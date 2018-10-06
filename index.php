1379dde25f15f0d9fb6ac46642ecf95a0ac445fe
Samkelo Gigaba <43130772+sgigaba@users.noreply.github.com>
2018-10-06 11:47:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+    <link rel = "stylesheet" type = "text/css" href = "styles/styles.css">
+    <title>indexpage</title>
+</head>
+<body class = "bc">
+<div class = "maindiv"> 
+<div class = "hclass">
+    <img id = "imgc" src = "http://webneel.com/sites/default/files/images/manual/logo/10-best-shopping-store-logo.gif"/>
+       <h1>Menubar</h1>
+</div>
+<div>   
+<div class = "contentc">
+        sidebar
+</div>
+    <div>
+        content area
+</div>
+<div>
+        footer
+    </div>
+</div>
+</div>
+</body>
+</html>
